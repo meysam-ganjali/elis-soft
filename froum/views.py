4e18631c0bec621ca6d44f15f38e8a70830d1512
meysam-ganjali <meysam.ghanjali1365@hotmail.com>
2024-04-18 18:08:41 +0100

    show all forum in html from backend

--- froum/views.py
+++ froum/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, redirect
 from django.views import View
+from .models import  Froum
 
 from account.models import CustomerUser
 
 
 class ForumView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'froum/forum.html')
+        forums = Froum.objects.all()
+        return render(request, 'froum/forum.html',{
+            'forums': forums
+        })
 
 
 class QuestionView(View):
